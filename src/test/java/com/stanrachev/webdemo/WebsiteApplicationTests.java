dba929c1567e003bc29215cb6706f32baf9a0cca
StanRachev <rachev.stanimir7@gmail.com>
2023-11-27 11:04:35 +0200

    Creating a WebSite

---
+++ src/test/java/com/stanrachev/webdemo/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanrachev.webdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
